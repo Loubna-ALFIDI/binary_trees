7ddee789ad8ad449788361f9ecbfdee9b22a4f72
HichamLuffy <imalxvssteve2020@gmail.com>
2023-11-30 01:47:42 +0100

    balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,15 +8,15 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left, right, balance;
-    if (tree == NULL)
-        return (0);
+	int left, right, balance;
+	if (tree == NULL)
+		return (0);
 
-    left = binary_tree_height(tree->left);
-    right = binary_tree_height(tree->right);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
-    balance = left - right;
-    return (balance);
+	balance = left - right;
+	return (balance);
 }
 
 /**
