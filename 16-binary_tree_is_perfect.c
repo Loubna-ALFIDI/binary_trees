bc2f257336daa893cd9215fde85c7883fc1da219
HichamLuffy <imalxvssteve2020@gmail.com>
2023-11-30 02:53:23 +0100

    is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -53,13 +53,14 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (binary_tree_balance(tree) == 0) 
+	if (binary_tree_balance(tree) == 0)
 	{
-		if (!tree->left && !tree->right) 
+		if (!tree->left && !tree->right)
 			return (1);
 		else
-			return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
-	}
-	return (0);
+			return (binary_tree_is_perfect(tree->left) &&
+					binary_tree_is_perfect(tree->right));
 
-}
+		return (0);
+
+	}
