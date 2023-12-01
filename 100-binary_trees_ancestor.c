ab2af6748d5873ebc9118ef10a6817f1ec6e4cbc
Loubna <loubna.alfidi@gmail.com>
2023-12-01 01:55:47 +0100

    Lowest common ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -5,35 +5,35 @@
  * @first: pointer to the first node
  * @second: pointer to the second node
  * Return: pointer to the lowest common ancestor node of the two given nodes
-*/
+ */
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-    binary_tree_t *path_first[100] = {NULL};
-    size_t path_first_length = 0;
+	binary_tree_t *path_first[100] = {NULL};
+	size_t path_first_length = 0;
 
-    if (first == NULL || second == NULL)
-        return NULL;
+	if (first == NULL || second == NULL)
+		return NULL;
 
-    if (first == second)
-        return (binary_tree_t *)first;
+	if (first == second)
+		return (binary_tree_t *)first;
 
-    while (first != NULL)
-    {
-        path_first[path_first_length++] = (binary_tree_t *)first;
-        first = first->parent;
-    }
+	while (first != NULL)
+	{
+		path_first[path_first_length++] = (binary_tree_t *)first;
+		first = first->parent;
+	}
 
-    while (second != NULL)
-    {
-        for (size_t i = 0; i < path_first_length; ++i)
-        {
-            if (path_first[i] == second)
-                return (binary_tree_t *)second;
-        }
+	while (second != NULL)
+	{
+		for (size_t i = 0; i < path_first_length; ++i)
+		{
+			if (path_first[i] == second)
+				return (binary_tree_t *)second;
+		}
 
-        second = second->parent;
-    }
+		second = second->parent;
+	}
 
-    return (NULL);
+	return (NULL);
 }
